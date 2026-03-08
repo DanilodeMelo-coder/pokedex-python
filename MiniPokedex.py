22b87471afa7cfed9ec570aaef27e926d6ff4901
DanilodeMelo-coder <danilo.demelo.0207@gmail.com>
2026-03-08 00:27:43 -0300

    feat: refactor pokedex structure (v2)

--- MiniPokedex.py
+++ MiniPokedex.py
@@ -5,34 +5,53 @@ class Pokemon:
         self.tipo = tipo
         self.nivel = nivel
 
+    #retorna os atributos da classe pokemon de uma forma mais limpa e organizada como Str
     def __str__(self):
         return f"{self.nome} | Tipo: {self.tipo} | HP: {self.hp} | Nivel: {self.nivel}"
 
 class Pokedex:
+    #iniciar pokedex
     def __init__(self):
         self.pokemons = []
 
-    def adicionar_pokemon(self):
+    #adicionar pokemon (input- Nome, Hp, Tipo, Nivel)
+    def adicionar_pokemon(self,nome_pk, hp_pk, tipo_pk, nivel_pk):
 
-        nome_pk = input("Adicione o nome do pokemon: ")
-        hp_pk = input("Adicione o hp do pokemon: ")
-        tipo_pk = input("Adicione o tipo do pokemon: ")
-        nivel_pk = input("Adicione o nivel do pokemon: ")
+        pokemon_adicionado = Pokemon(nome_pk, hp_pk, tipo_pk, nivel_pk)
+        pokemon_nome = nome_pk
+        
+        #retorna o Objeto Pokemon e o nome do Pokemon
+        return pokemon_nome, pokemon_adicionado
 
-        dados = nome_pk, hp_pk, tipo_pk, nivel_pk
-        return dados
-
+    #listar pokemon
     def listar_pokemons(self):
         for pokemon in self.pokemons:
             print(pokemon)
-    
-    def buscar_pokemon(self):
-        ...
-    
-    def mostrar_detalhes(self, nome):
-        ...
+
+    #buscar pokemon pelo nome (input- nome do pokemon)
+    def buscar_pokemon(self, nome_pk):
+
+        for pokemon in self.pokemons:
+            #se pokemon.nome for = nome digitado
+            if pokemon.nome == nome_pk:
+                #retorne pokemon
+                return pokemon
+            
+            #se nao, retorne None
+            return None
+
+    #mostrar detalhes do pokemon, pelo nome
+    def mostrar_detalhes(self, nome_pk):
+        
+        for pokemon in self.pokemons:
+            if pokemon.nome == nome_pk:
+
+                return pokemon
+
+            return None
 
 
+#menu inicial
 def menu():
     print("1- Adicionar Pokemon")
     print("2- Exibir pokemons")
@@ -40,12 +59,13 @@ def menu():
     print("4- Exibir informações do Pokemon")
     print("5- Sair")
 
+#entrada do user para escolher uma opção do menu
 def entrada_user():
     resposta_user = input("Escolha uma Opção: ")
 
     return resposta_user
 
-
+#função que executa tudo
 def executar():
     pokedex = Pokedex()
 
@@ -55,15 +75,50 @@ def executar():
         resposta_user = entrada_user()
 
         if resposta_user == "1":
+            nome_pk = input("Adicione o nome do pokemon: ")
+            hp_pk = input("Adicione o hp do pokemon: ")
+            tipo_pk = input("Adicione o tipo do pokemon: ")
+            nivel_pk = input("Adicione o nivel do pokemon: ")
 
-            nome_pk, hp_pk, tipo_pk, nivel_pk = pokedex.adicionar_pokemon()
+            pokemon_adicionado_nome, pokemon_adicionado = pokedex.adicionar_pokemon(nome_pk, hp_pk, tipo_pk, nivel_pk)
+            pokedex.pokemons.append(pokemon_adicionado)
 
-            pokemon_nome = Pokemon(nome_pk, hp_pk, tipo_pk, nivel_pk)
-
-            pokedex.pokemons.append(pokemon_nome)
+            print("-------------------------------------------")
+            print(f"{pokemon_adicionado_nome} adicionado com sucesso a Pokedéx")
+            print("-------------------------------------------")
 
         elif resposta_user == "2":
+            print("----------------------POKEDEX----------------------")
             pokedex.listar_pokemons()
+            print("---------------------------------------------------")
+
+        elif resposta_user == "3":
+            nome_pk = input("Digite o nome do pokemon: ").lower().strip()
+
+            pokemon = pokedex.buscar_pokemon(nome_pk)
+
+            if pokemon:
+                print("----------------------POKEDEX----------------------")
+                print(f"{nome_pk} ja esta registrado na pokedex")
+                print("---------------------------------------------------")
+            
+            else:
+                print(f"{nome_pk} ainda não esta registrado na pokedex")
+
+        elif resposta_user == "4":
+            nome_pk = input("Digite o nome do pokemon que busca: ")
+
+            pokemon = pokedex.mostrar_detalhes(nome_pk)
+
+            if pokemon is not None:
+                print("----------------------POKEDEX----------------------")
+                print(pokemon)
+                print("---------------------------------------------------")
+            
+            else:
+                print("----------------------POKEDEX----------------------")
+                print("Pokemon não encontrado")
+                print("---------------------------------------------------")
 
         elif resposta_user == "5":
             break
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# pokedex-python
-Projeto simples de Pokédex em Python rodando no terminal. Permite criar e listar Pokémons usando classes, orientação a objetos e um menu interativo no console.
+Pokedex Python (Projeto de Estudo)
+
+Este projeto é uma Pokedex simples feita em Python, criada como exercício prático para iniciar os estudos em Programação Orientada a Objetos (POO).
+
+A aplicação funciona via terminal, permitindo adicionar e listar pokémons.
+
+-- Objetivo do projeto
+
+Praticar conceitos fundamentais como:
+
+Classes
+
+Métodos
+
+Organização de código
+
+Fluxo de execução
+
+Estrutura de projetos Python
+
+-- Funcionalidades atuais
+
+Adicionar pokémons
+
+Listar pokémons
+
+Menu interativo no terminal
+
+-- Melhorias planejadas
+
+Criar uma classe Pokemon 🗸
+
+Buscar pokémon pelo nome 🗸
+
+Exibir detalhes de um pokémon específico 🗸
+
+Separar o projeto em múltiplos arquivos (pokemon.py, pokedex.py, main.py)
+
+Melhorar a organização do código
+
+Adicionar persistência de dados futuramente (Json)
+
+Este projeto faz parte do meu processo de aprendizado em Python e desenvolvimento de software.
